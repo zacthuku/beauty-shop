baf36b2fce86ad3da11a67876b6a97c81489d808
Marycharity <marycharity5@gmail.com>
2025-07-17 11:26:49 +0300

    Add cart route

--- backend/app.py
+++ backend/app.py
@@ -18,6 +18,7 @@ app.register_blueprint(user_bp)
 app.register_blueprint(product_bp)
 app.register_blueprint(order_bp)
 app.register_blueprint(category_bp)
+app.register_blueprint(cart_bp)
 
 @app.route('/')
 def index():
